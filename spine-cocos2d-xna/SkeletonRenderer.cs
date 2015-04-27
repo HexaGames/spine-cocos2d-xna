857bd873b9fbd8641646b927a5a3524611f8d39f
yuuhhe <yuuhhe@gmail.com>
2015-04-27 16:09:28 +0800

    SkeletonRenderer

--- spine-cocos2d-xna/SkeletonRenderer.cs
+++ spine-cocos2d-xna/SkeletonRenderer.cs
@@ -1,167 +1,381 @@
-﻿using Cocos2D;
+﻿using System;
+using Cocos2D;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Spine
 {
-    public class SkeletonRenderer: CCNodeRGBA, ICCBlendProtocol
+    public class SkeletonRenderer : CCNodeRGBA, ICCBlendProtocol
     {
+
+        static int[] quadTriangles = { 0, 1, 2, 2, 3, 0 };
+
         private bool ownsSkeletonData;
         private Atlas atlas;
         private PolygonBatch batch;
-        private float worldVertices;
-        
-        
-	public Skeleton skeleton;
-	public Bone rootBone;
-public	float timeScale;
-public	bool debugSlots;
-public	bool debugBones;
-public	bool premultipliedAlpha;
+        private float[] worldVertices;
 
-        private void initialize()
+        public Skeleton skeleton;
+        public Bone rootBone;
+        public float timeScale;
+        public bool debugSlots;
+        public bool debugBones;
+        public bool premultipliedAlpha;
+
+        private void Initialize()
         {
+            debugSlots = false;
+            debugBones = false;
+            timeScale = 1;
+
+            worldVertices = new float[1000]; // Max number of vertices per mesh.
+
+            batch = PolygonBatch.CreateWithCapacity(2000); // Max number of vertices and triangles per batch.
+
+            //blendFunc.src = GL_ONE;
+            //blendFunc.dst = GL_ONE_MINUS_SRC_ALPHA;
+            BlendFunc = new CCBlendFunc(CCOGLES.GL_ONE, CCOGLES.GL_ONE_MINUS_SRC_ALPHA);
+
+            SetOpacityModifyRGB(true);
+
+            //SetShaderProgram(CCShaderCache::sharedShaderCache()->programForKey(kCCShader_PositionTextureColor));
+            ScheduleUpdate();
         }
 
         protected SkeletonRenderer()
         {
-            
-        }
-
-        protected void SetSkeletonData(SkeletonData skeletonData, bool ownsSkeletonData)
-        {
-            
-        }
-
-        protected virtual CCTexture2D getTexture(RegionAttachment attachment)
-        {
-            
-        }
-
-        protected virtual CCTexture2D getTexture(MeshAttachment attachment)
-        {
-            
-        }
-
-        protected virtual CCTexture2D getTexture(SkinnedMeshAttachment attachment)
-        {
-            
-        }
-
-        public static SkeletonRenderer createWithData(SkeletonData skeletonData, bool ownsSkeletonData = false)
-        {
-            
-        }
-
-        public static SkeletonRenderer createWithFile(string skeletonDataFile, Atlas atlas, float scale = 0)
-        {
-            
-        }
-
-        public static SkeletonRenderer createWithFile(string skeletonDataFile, string atlasFile, float scale = 0)
-        {
-            
+            Initialize();
         }
 
         public SkeletonRenderer(SkeletonData skeletonData, bool ownsSkeletonData = false)
         {
-            
+            Initialize();
+            SetSkeletonData(skeletonData, ownsSkeletonData);
         }
 
         public SkeletonRenderer(string skeletonDataFile, Atlas atlas, float scale = 0)
         {
-            
+            Initialize();
+
+            SkeletonJson json = new SkeletonJson(atlas);
+            json.Scale = scale;
+            SkeletonData skeletonData = json.ReadSkeletonData(skeletonDataFile);
+            //CCAssert(skeletonData, json->error ? json->error : "Error reading skeleton data.");
+            //spSkeletonJson_dispose(json);
+
+            SetSkeletonData(skeletonData, true);
         }
 
-        public SkeletonRenderer(string skeletonDataFile, string atlasFile, float scale = 0)
+        //public SkeletonRenderer(string skeletonDataFile, string atlasFile, float scale = 0)
+        //{
+        //    Initialize();
+
+        //    atlas = new Atlas(atlasFile, new XnaTextureLoader(GraphicsDevice));
+        //    //CCAssert(atlas, "Error reading atlas file.");
+
+        //    SkeletonJson json = new SkeletonJson(atlas);
+        //    json.Scale = scale;
+        //    SkeletonData skeletonData = json.ReadSkeletonData(skeletonDataFile);
+        //    //CCAssert(skeletonData, json->error ? json->error : "Error reading skeleton data.");
+        //    //spSkeletonJson_dispose(json);
+
+        //    SetSkeletonData(skeletonData, true);
+        //}
+
+        protected void SetSkeletonData(SkeletonData skeletonData, bool ownsSkeletonData)
         {
-            
+            skeleton = new Skeleton(skeletonData);
+            rootBone = skeleton.Bones[0];
+            this.ownsSkeletonData = ownsSkeletonData;
         }
 
-       public  virtual void Update(float deltaTime)
+        protected virtual CCTexture2D getTexture(RegionAttachment attachment)
         {
-            
+            return (CCTexture2D)((AtlasRegion)attachment.RendererObject).page.rendererObject;
+        }
+
+        protected virtual CCTexture2D getTexture(MeshAttachment attachment)
+        {
+            return (CCTexture2D)((AtlasRegion)attachment.RendererObject).page.rendererObject;
+        }
+
+        protected virtual CCTexture2D getTexture(SkinnedMeshAttachment attachment)
+        {
+            return (CCTexture2D)((AtlasRegion)attachment.RendererObject).page.rendererObject;
+        }
+
+        public static SkeletonRenderer CreateWithData(SkeletonData skeletonData, bool ownsSkeletonData = false)
+        {
+            SkeletonRenderer node = new SkeletonRenderer(skeletonData, ownsSkeletonData);
+            return node;
+        }
+
+        public static SkeletonRenderer CreateWithFile(string skeletonDataFile, Atlas atlas, float scale = 0)
+        {
+            SkeletonRenderer node = new SkeletonRenderer(skeletonDataFile, atlas, scale);
+            return node;
+        }
+
+        //public static SkeletonRenderer CreateWithFile(string skeletonDataFile, string atlasFile, float scale = 0)
+        //{
+        //    SkeletonRenderer node = new SkeletonRenderer(skeletonDataFile, atlasFile, scale);
+        //    return node;
+        //}
+
+        public virtual void Update(float deltaTime)
+        {
+            skeleton.Update(deltaTime * timeScale);
         }
 
         public virtual void Draw()
         {
-            
+            //CC_NODE_DRAW_SETUP();
+            //ccGLBindVAO(0);
+
+            CCColor3B nodeColor = Color;
+            skeleton.R = nodeColor.R / (float)255;
+            skeleton.G = nodeColor.G / (float)255;
+            skeleton.B = nodeColor.B / (float)255;
+            skeleton.A = DisplayedOpacity / (float)255;
+
+            int blendMode = -1;
+            CCColor4B color;
+            float[] uvs = null;
+            int verticesCount = 0;
+            int[] triangles = null;
+            int trianglesCount = 0;
+            float r = 0, g = 0, b = 0, a = 0;
+            for (int i = 0, n = skeleton.Slots.Count; i < n; i++)
+            {
+                Slot slot = skeleton.DrawOrder[i];
+                if (slot.Attachment == null)
+                    continue;
+                CCTexture2D texture = null;
+                if (slot.Attachment is RegionAttachment)
+                {
+                    RegionAttachment attachment = (RegionAttachment)slot.Attachment;
+                    attachment.ComputeWorldVertices(slot.Bone, worldVertices);
+                    texture = getTexture(attachment);
+                    uvs = attachment.UVs;
+                    verticesCount = 8;
+                    triangles = quadTriangles;
+                    trianglesCount = 6;
+                    r = attachment.R;
+                    g = attachment.G;
+                    b = attachment.B;
+                    a = attachment.A;
+                }
+                else if (slot.Attachment is MeshAttachment)
+                {
+                    MeshAttachment attachment = (MeshAttachment)slot.Attachment;
+                    attachment.ComputeWorldVertices(slot, worldVertices);
+                    texture = getTexture(attachment);
+                    uvs = attachment.UVs;
+                    verticesCount = attachment.Vertices.Length;
+                    triangles = attachment.Triangles;
+                    trianglesCount = attachment.Triangles.Length;
+                    r = attachment.R;
+                    g = attachment.G;
+                    b = attachment.B;
+                    a = attachment.A;
+                }
+                else if (slot.Attachment is SkinnedMeshAttachment)
+                {
+                    SkinnedMeshAttachment attachment = (SkinnedMeshAttachment)slot.Attachment;
+                    attachment.ComputeWorldVertices(slot, worldVertices);
+                    texture = getTexture(attachment);
+                    uvs = attachment.UVs;
+                    verticesCount = attachment.UVs.Length;
+                    triangles = attachment.Triangles;
+                    trianglesCount = attachment.Triangles.Length;
+                    r = attachment.R;
+                    g = attachment.G;
+                    b = attachment.B;
+                    a = attachment.A;
+                }
+                if (texture != null)
+                {
+                    if ((int)slot.Data.BlendMode != blendMode)
+                    {
+                        batch.flush();
+                        blendMode = (int)slot.Data.BlendMode;
+                        switch (slot.Data.BlendMode)
+                        {
+                            //UNDONE BlendMode
+                            case BlendMode.additive:
+                                //CCGLBlendFunc(premultipliedAlpha ? GL_ONE : GL_SRC_ALPHA, GL_ONE);
+                                break;
+                            case BlendMode.multiply:
+                                //ccGLBlendFunc(GL_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA);
+                                break;
+                            case BlendMode.screen:
+                                //ccGLBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_COLOR);
+                                break;
+                            default:
+                                //ccGLBlendFunc(blendFunc.src, blendFunc.dst);
+                                break;
+                        }
+                    }
+                    color.A = (byte)(skeleton.A * slot.A * a * 255);
+                    float multiplier = premultipliedAlpha ? color.A : 255;
+                    color.R = (byte)(skeleton.R * slot.R * r * multiplier);
+                    color.G = (byte)(skeleton.G * slot.G * g * multiplier);
+                    color.B = (byte)(skeleton.B * slot.B * b * multiplier);
+                    batch.add(texture, worldVertices, uvs, verticesCount, triangles, trianglesCount, color);
+                }
+            }
+            batch.flush();
+
+            //if (debugSlots) {
+            //    // Slots.
+            //    ccDrawColor4B(0, 0, 255, 255);
+            //    glLineWidth(1);
+            //    CCPoint points[4];
+            //    for (int i = 0, n = skeleton->slotsCount; i < n; i++) {
+            //        spSlot* slot = skeleton->drawOrder[i];
+            //        if (!slot->attachment || slot->attachment->type != SP_ATTACHMENT_REGION) continue;
+            //        spRegionAttachment* attachment = (spRegionAttachment*)slot->attachment;
+            //        spRegionAttachment_computeWorldVertices(attachment, slot->bone, worldVertices);
+            //        points[0] = ccp(worldVertices[0], worldVertices[1]);
+            //        points[1] = ccp(worldVertices[2], worldVertices[3]);
+            //        points[2] = ccp(worldVertices[4], worldVertices[5]);
+            //        points[3] = ccp(worldVertices[6], worldVertices[7]);
+            //        ccDrawPoly(points, 4, true);
+            //    }
+            //}
+            //if (debugBones) {
+            //    // Bone lengths.
+            //    glLineWidth(2);
+            //    ccDrawColor4B(255, 0, 0, 255);
+            //    for (int i = 0, n = skeleton->bonesCount; i < n; i++) {
+            //        spBone *bone = skeleton->bones[i];
+            //        float x = bone->data->length * bone->m00 + bone->worldX;
+            //        float y = bone->data->length * bone->m10 + bone->worldY;
+            //        ccDrawLine(ccp(bone->worldX, bone->worldY), ccp(x, y));
+            //    }
+            //    // Bone origins.
+            //    ccPointSize(4);
+            //    ccDrawColor4B(0, 0, 255, 255); // Root bone is blue.
+            //    for (int i = 0, n = skeleton->bonesCount; i < n; i++) {
+            //        spBone *bone = skeleton->bones[i];
+            //        ccDrawPoint(ccp(bone->worldX, bone->worldY));
+            //        if (i == 0) ccDrawColor4B(0, 255, 0, 255);
+            //    }
+            //}
         }
 
         public virtual CCRect BoundingBox()
         {
-            
+            float minX = float.MaxValue, minY = float.MaxValue, maxX = float.MinValue, maxY = float.MinValue;
+            float scaleX = ScaleX, scaleY = ScaleY;
+            for (int i = 0; i < skeleton.Slots.Count; ++i)
+            {
+                Slot slot = skeleton.Slots[i];
+                if (slot.Attachment == null)
+                    continue;
+
+                int verticesCount;
+                if (slot.Attachment is RegionAttachment)
+                {
+                    RegionAttachment attachment = (RegionAttachment)slot.Attachment;
+                    attachment.ComputeWorldVertices(slot.Bone, worldVertices);
+                    verticesCount = 8;
+                }
+                else if (slot.Attachment is MeshAttachment)
+                {
+                    MeshAttachment attachment = (MeshAttachment)slot.Attachment;
+                    attachment.ComputeWorldVertices(slot, worldVertices);
+                    verticesCount = attachment.Vertices.Length;
+                }
+                else if (slot.Attachment is SkinnedMeshAttachment)
+                {
+                    SkinnedMeshAttachment attachment = (SkinnedMeshAttachment)slot.Attachment;
+                    attachment.ComputeWorldVertices(slot, worldVertices);
+                    verticesCount = attachment.UVs.Length;
+                }
+                else
+                    continue;
+
+                for (int ii = 0; ii < verticesCount; ii += 2)
+                {
+                    float x = worldVertices[ii] * scaleX, y = worldVertices[ii + 1] * scaleY;
+                    minX = Math.Min(minX, x);
+                    minY = Math.Min(minY, y);
+                    maxX = Math.Max(maxX, x);
+                    maxY = Math.Max(maxY, y);
+                }
+            }
+            CCPoint position = Position;
+            return new CCRect(position.X + minX, position.Y + minY, maxX - minX, maxY - minY);
         }
 
-	// --- Convenience methods for common Skeleton_* functions.
+        // --- Convenience methods for common Skeleton_* functions.
         public void UpdateWorldTransform()
         {
-            
+            skeleton.UpdateWorldTransform();
         }
 
         public void SetToSetupPose()
         {
-            
+            skeleton.SetToSetupPose();
         }
 
         public void SetBonesToSetupPose()
         {
-            
+            skeleton.SetBonesToSetupPose();
         }
 
         public void SetSlotsToSetupPose()
         {
-            
+            skeleton.SetSlotsToSetupPose();
         }
 
-	/* Returns 0 if the bone was not found. */
+        /* Returns 0 if the bone was not found. */
         public Bone FindBone(string boneName)
         {
-            
+            return skeleton.FindBone(boneName);
         }
 
-	/* Returns 0 if the slot was not found. */
-        public Slot findSlot(string slotName)
+        /* Returns 0 if the slot was not found. */
+        public Slot FindSlot(string slotName)
         {
-            
+            return skeleton.FindSlot(slotName);
         }
-	
-	/* Sets the skin used to look up attachments not found in the SkeletonData defaultSkin. Attachments from the new skin are
-	 * attached if the corresponding attachment from the old skin was attached. If there was no old skin, each slot's setup mode
-	 * attachment is attached from the new skin. Returns false if the skin was not found.
-	 * @param skin May be 0.*/
+
+        /* Sets the skin used to look up attachments not found in the SkeletonData defaultSkin. Attachments from the new skin are
+         * attached if the corresponding attachment from the old skin was attached. If there was no old skin, each slot's setup mode
+         * attachment is attached from the new skin. Returns false if the skin was not found.
+         * @param skin May be 0.*/
         public bool SetSkin(string skinName)
         {
-            
+            skeleton.SetSkin(skinName);
+            return true;
         }
 
-	/* Returns 0 if the slot or attachment was not found. */
-        public Attachment getAttachment(string slotName, string attachmentName)
+        /* Returns 0 if the slot or attachment was not found. */
+        public Attachment GetAttachment(string slotName, string attachmentName)
         {
-            
+            return skeleton.GetAttachment(slotName, attachmentName);
         }
 
-	/* Returns false if the slot or attachment was not found. */
+        /* Returns false if the slot or attachment was not found. */
         public bool SetAttachment(string slotName, string attachmentName)
         {
-            
+            skeleton.SetAttachment(slotName, attachmentName);
+            return true;
         }
 
-	// --- BlendProtocol
-	public CC_PROPERTY(cocos2d::ccBlendFunc, blendFunc, BlendFunc);
-	public virtual void setOpacityModifyRGB (bool value);
-	public virtual bool isOpacityModifyRGB ();
-
-
-
-        public CCBlendFunc BlendFunc
+        // --- BlendProtocol
+        public virtual void SetOpacityModifyRGB(bool value)
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-                throw new System.NotImplementedException();
-            }
+            premultipliedAlpha = value;
         }
+
+        public virtual bool IsOpacityModifyRGB()
+        {
+            return premultipliedAlpha;
+        }
+
+        //public CC_PROPERTY(cocos2d::ccBlendFunc, blendFunc, BlendFunc);
+        public CCBlendFunc BlendFunc { get; set; }
     }
 }
